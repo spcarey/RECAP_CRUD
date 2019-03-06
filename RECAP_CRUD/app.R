45ad5b3c5fc0f1051a1aea45517115d5aa459451
careys <careys@forcenexus.io>
2019-03-06 14:40:49 -0500

    * UI boxes complete

---
+++ RECAP_CRUD/app.R
@@ -0,0 +1,174 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinydashboard)
+library(DT)
+
+
+#DATABASE CONNECTION POOL
+# Defines the parameters for connecting to the database 
+recapdb <- dbPool(
+  
+  drv = RMySQL::MySQL(),
+  dbname = "recap",
+  host = "127.0.0.1",
+  port = 3306,
+  username = "root",
+  password = "!QAZ2wsx"
+  
+)
+
+fleet_info <- recapdb %>%
+  tbl("Fleet_Info") %>%
+  dplyr::collect() 
+
+
+ui <- dashboardPage(
+  dashboardHeader(title = "FLIGHT LOG"),
+  dashboardSidebar(
+    sidebarMenu( #DEFINE SIDE BAR NAV ICONS
+      menuItem("QUERY LOG", tabName = "SELECT", icon = icon("database")),
+      menuItem("ADD TO LOG", tabName = "INSERT", icon = icon("plus")),
+      menuItem("UPDATE LOG ENTRY", tabName = "UPDATE", icon = icon("user-edit")),
+      menuItem("DELETE LOG ENTRY", tabName = "DELETE", icon = icon("backspace"))
+    )
+  ),  #BEGIN BODY CONTENT
+  dashboardBody(
+    tabItems(
+      # BEGIN SELECT TAB
+      tabItem(
+        tabName = "SELECT",
+        h2("  Query Flight Log"),
+        fluidRow(
+          # BEGIN FIRST COLUMN in SELECT TAB
+          column(width = 4,
+                 wellPanel(
+                   #WIDGET FOR SELECTING AIRCRAFT
+                   selectInput("select_tail",
+                               "Select Tail Number", 
+                               choices = c(Choose="", as.list(fleet_info$Tail_Num)),
+                               selected = "Sleepy",
+                               selectize = TRUE),
+                   #WIDGET FOR SELECTING DATES
+                   dateRangeInput('select_dateRange',
+                                  label = 'Select Date',
+                                  start = '1/1/1995', end = Sys.Date(),
+                                  format = 'm/d/yyyy'),
+                   actionButton("select_button", "View Log" )
+                   
+                   
+                 )
+            
+          ),  #END FIRST COLUMN INSERT TAB
+              #BEGIN SECOND COLUMN INSERT TAB
+          column( width = 8
+                  
+                 #datatableOutput
+          )
+        )
+      ),# END SELECT TAB
+        # BEGIN INSERT TAB
+      tabItem(
+        tabName = "INSERT",
+        h2("Create New Log Entry"),
+        fluidRow(
+          # BEGIN FIRST COLUMN in INSERT TAB
+          column(width = 4,
+                 wellPanel(
+                   #WIDGET FOR SELECTING AIRCRAFT
+                   selectInput("insert_tail",
+                               "Select Tail Number", 
+                               choices = c(Choose="", as.list(fleet_info$Tail_Num)),
+                               selected = "Sleepy",
+                               selectize = TRUE),
+                   #WIDGET FOR SELECTING DATES
+                   dateInput("insert_date", "Select Date: ", value = Sys.Date(), format = "m/d/yyyy"),
+                   numericInput("hours_insert", label = "Enter # of Flight Hours:", value = 0, min = 0),
+                   actionButton("insert_button", "Add to Log" )
+                   
+                   
+                 )
+                 
+          ),  #END FIRST COLUMN SELECT TAB
+              #BEGIN SECOND COLUMN SELECT TAB
+          column( width = 8
+                  
+                  #datatableOutput
+          )
+        )
+      ), #END INSERT TAB
+         #Begin UPDATE TAB
+      
+      tabItem(
+        tabName = "UPDATE",
+        h2("Update Previous Log Entry"),
+        fluidRow(
+          # BEGIN FIRST COLUMN in UPDATE TAB
+          column(width = 4,
+                 wellPanel(
+                   #WIDGET FOR SELECTING AIRCRAFT
+                   selectInput("update_tail",
+                               "Select Tail Number", 
+                               choices = c(Choose="", as.list(fleet_info$Tail_Num)),
+                               selected = "Sleepy",
+                               selectize = TRUE),
+                   #WIDGET FOR SELECTING DATES
+                   dateInput("update_date", "Select Date: ", value = Sys.Date(), format = "m/d/yyyy"),
+                   numericInput("hours_update", label = "Enter # of Flight Hours:", value = 0, min = 0),
+                   actionButton("update_button", "Edit Log" )
+                   
+                   
+                 )
+                 
+          ),  #END FIRST COLUMN UPDATE TAB
+          #BEGIN SECOND COLUMN UPDATE TAB
+          column( width = 8
+                  
+                  #datatableOutput
+          )
+        )
+      ), # END UPDATE TAB
+      
+      tabItem(
+        tabName = "DELETE",
+        h2("Delete Log Entry"),
+        fluidRow(
+          # BEGIN FIRST COLUMN in INSERT TAB
+          column(width = 4,
+                 wellPanel(
+                   #WIDGET FOR SELECTING AIRCRAFT
+                   selectInput("delete_tail",
+                               "Select Tail Number", 
+                               choices = c(Choose="", as.list(fleet_info$Tail_Num)),
+                               selected = "Sleepy",
+                               selectize = TRUE),
+                   #WIDGET FOR SELECTING DATES
+                   dateInput("delete_date", "Select Date: ", value = Sys.Date(), format = "m/d/yyyy"),
+                   actionButton("delete_button", "Add to Log" )
+                   
+                   
+                 )
+                 
+          ),  #END FIRST COLUMN DELETE TAB
+          #BEGIN SECOND COLUMN DELETE TAB
+          column( width = 8
+                  
+                  #datatableOutput
+          )
+        )
+      )
+    )
+  ) # END BODY CONTENT
+)
+
+server <- function(input, output) { }
+
+shinyApp(ui, server)
+
---
+++ populateDB.R
@@ -0,0 +1,105 @@
+library(pool)
+library(dplyr)
+
+recapdb <- dbPool(
+   
+   drv = RMySQL::MySQL(),
+   dbname = "recap",
+   host = "127.0.0.1",
+   port = 3306,
+   username = "root",
+   password = "!QAZ2wsx"
+   
+)
+
+copy_to(
+   dest = recapdb,
+   name = "Sleepy",
+   df = Sleepy_Usage_Report,
+   temporary = FALSE,
+   overwrite = TRUE
+   ) 
+
+copy_to(
+   dest = recapdb,
+   name = "Grumpy",
+   df = Grumpy_Usage_Report,
+   temporary = FALSE,
+   overwrite = TRUE
+) 
+
+copy_to(
+   dest = recapdb,
+   name = "Doc",
+   df = Doc_Usage_Report,
+   temporary = FALSE,
+   overwrite = TRUE
+) 
+
+copy_to(
+   dest = recapdb,
+   name = "Sneezy",
+   df = Sneezy_Usage_Report,
+   temporary = FALSE,
+   overwrite = TRUE
+) 
+
+copy_to(
+   dest = recapdb,
+   name = "Fleet_Info",
+   df = Fleet_Info,
+   temporary = FALSE,
+   overwrite = TRUE
+) 
+
+tail <- "Doc"
+date <- "2/2/2019"
+hours <-"6" 
+
+
+sql_code <- paste0("INSERT INTO"," ",tail," ","(Date , Hours)"," ","VALUES"," ","(","\'",date,"\'",",","\'",hours,"\'",")")
+
+cat(sql_code)
+
+
+query <- sqlInterpolate(recapdb, sql_code )
+
+dbGetQuery(recapdb, sql_code)
+
+recapdb %>%
+  tbl("Sleepy") %>%
+  dplyr::collect() %>% tail()
+
+
+
+
+killDbConnections <- function () {
+  
+  all_cons <- dbListConnections(RMySQL::MySQL())
+  
+  print(all_cons)
+  
+  for(con in all_cons)
+    +  dbDisconnect(con)
+  
+  print(paste(length(all_cons), " connections killed."))
+  
+}
+
+RMySQL::dbDisconnect(RMySQL::dbListConnections(RMySQL::MySQL())[[1]])
+
+
+
+sidebarPanel(selectInput("tail_db", h4(em(p(strong("Please select a tail number."))), style = "color: green" ), 
+                         choices = c(Choose="", as.list(fleet.data$Tail_Num)),
+                         selected = "Sleepy",
+                         selectize = TRUE),
+             dateInput("date_db", "Select Date: ", value = Sys.Date(), format = "m/d/yyyy"),
+             
+             numericInput("hours_db", label = "Enter # of Flight Hours:", value = 0, min = 0),
+             actionButton("create_db", "Add to Log" )
+), ##/SidebarPanel
+mainPanel(
+  verbatimTextOutput("SQL_INSERT")
+) ##/MainPanel 
+
